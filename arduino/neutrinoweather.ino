01ca97064a2e2a9402f0e76d0d721442ab0992a8
Marcus Sorensen <marcus@betterservers.com>
2014-07-14 17:57:19 -0600

    avr code updates for encryption, moved pin definitions to constants

--- arduino/neutrinoweather.ino
+++ arduino/neutrinoweather.ino
@@ -7,23 +7,52 @@
 #include <avr/sleep.h>
 #include <avr/eeprom.h>
 #include <LowPower.h>
+#include <skipjack.h>
 
 #define EEPROM_KEY_ADDR 0x00
 #define EEPROM_KEY_SIZE 10
 
-int rfgoodled = 3;
-int rfbadled  = 4;
-int lobattled = 2;
+#define CHANNEL_PIN_0 0
+#define CHANNEL_PIN_1 1
+#define CHANNEL_PIN_2 10
+#define CHANNEL_PIN_3 A3
+
+#define ADDRESS_PIN_0 5
+#define ADDRESS_PIN_1 6
+#define ADDRESS_PIN_2 7
+
+#define CHANNEL_PIN_0 0
+#define CHANNEL_PIN_1 1
+#define CHANNEL_PIN_2 10
+#define CHANNEL_PIN_3 A3
+
+#define ENCRYPT_PIN A7
+
+#define RF_GOOD_LED 3
+#define RF_BAD_LED 4
+#define LO_BATT_LED 2
 
 int myaddr = getMyAddr();
+int mychannel = getMyChannel();
 int32_t lastmillivolts = 0;
 
-struct weather {
-    int8_t  addr = myaddr;
-    int16_t tempc = 0;
-    int16_t humidity = 0;
-    int32_t pressurep = 0;
-    int32_t millivolts = 0;
+// this struct should always be a multiple of 64 bits so we can easily encrypt it (skipjack 64bit blocks)
+struct sensordata {
+    int8_t  addr         = myaddr;
+    int8_t  placeholder1 = 0; // future var
+    int16_t tempc        = 0;
+    int16_t humidity     = 0;
+    int16_t placeholder2 = 0; // future var
+    int32_t pressurep    = 0;
+    int32_t millivolts   = 0;
+};
+
+// 28 bytes. Want to keep this <= 32 bytes so that it will fit in one radio packet
+struct message {
+    int8_t addr = myaddr; // 1 byte
+    boolean encrypted = false; // 1 byte
+    byte key[EEPROM_KEY_SIZE] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // 10 bytes
+    sensordata s; // 16 bytes
 };
 
 RF24 radio(8,9);
@@ -36,9 +65,8 @@ byte key[EEPROM_KEY_SIZE];
 void setup() {
     // populate encryption key
     eeprom_read_block((void*)&key, (const void*)EEPROM_KEY_ADDR, sizeof(key));
-    if (key_is_empty(key)) {
-        byte newkey[EEPROM_KEY_SIZE];
-        generate_key(key);
+    if (keyIsEmpty(key)) {
+        generateKey(key);
         eeprom_write_block((const void*)&key, (void*)EEPROM_KEY_ADDR, EEPROM_KEY_SIZE);
     }
     
@@ -52,27 +80,27 @@ void setup() {
     MCUCR = bit (BODS); 
     sleep_cpu ();
   
-    pinMode(rfgoodled,OUTPUT);
-    pinMode(rfbadled,OUTPUT);
-    pinMode(lobattled,OUTPUT);
+    pinMode(RF_GOOD_LED,OUTPUT);
+    pinMode(RF_BAD_LED,OUTPUT);
+    pinMode(LO_BATT_LED,OUTPUT);
   
     //flash lights on boot
-    flash(rfgoodled);
+    flash(RF_GOOD_LED);
     LowPower.powerDown(SLEEP_120MS, ADC_OFF, BOD_OFF);
-    flash(rfbadled);
+    flash(RF_BAD_LED);
     LowPower.powerDown(SLEEP_120MS, ADC_OFF, BOD_OFF);
-    flash(rfgoodled);
+    flash(RF_GOOD_LED);
   
     //flash node address
     LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
     for (int i = 0 ; i < myaddr; i++) {
-        pulse(rfgoodled);   
+        pulse(RF_GOOD_LED);   
     }
   
     radio.begin();
     radio.setRetries(10,10);
     radio.setPayloadSize(16);
-    radio.setChannel(0x4c);
+    radio.setChannel(mychannel);
     radio.setDataRate(RF24_250KBPS);
     radio.setPALevel(RF24_PA_MAX);
     radio.openWritingPipe(pipe);
@@ -84,42 +112,54 @@ void setup() {
 }
 
 void loop() {
-    weather w;
+    message m;
     // humidity, temp
     if (hsensor.sensorExists()) {
         si7021_env sidata = hsensor.getHumidityAndTemperature();
-        w.humidity        = sidata.humidityBasisPoints;
-        w.tempc           = sidata.celsiusHundredths;
+        m.s.humidity        = sidata.humidityBasisPoints;
+        m.s.tempc           = sidata.celsiusHundredths;
     }
     
     // pressure
     if (bsensor.sensorExists()) {
         // fall back to bosch sensor for temperature if necessary
         if(! hsensor.sensorExists()) {
-            w.tempc = bsensor.getCelsiusHundredths();
+            m.s.tempc = bsensor.getCelsiusHundredths();
         }
-        w.pressurep = bsensor.getPressurePascals();
+        m.s.pressurep = bsensor.getPressurePascals();
         
     }
     
     // voltage was read after last radio send to get reading after high power draw
-    w.millivolts = lastmillivolts;
+    m.s.millivolts = lastmillivolts;
+    
+    // when encryption is disabled, we send the key. when it is enabled, we send the empty key
+    if (shouldEncrypt()) {
+        m.encrypted = true;
+        // encrypt memory blocks containing sensor data. skipjack is 64 bit (8 byte) blocks
+        for (int i = 0; i < sizeof(m.s); i += 8) {
+            int ptrshift = i * 8;
+            skipjack_enc((&m.s + ptrshift),&key);
+        }
+    } else {
+        memcpy((void*)&m.key, (void*)&key, EEPROM_KEY_SIZE * sizeof(byte));
+    }
     
     radio.powerUp();
     delay(2);
     radio.stopListening();
-    bool ok = radio.write(&w, sizeof w);
+    bool ok = radio.write(&m, sizeof m);
     if (ok) {
-        flash(rfgoodled);
+        flash(RF_GOOD_LED);
     } else {
-        flash(rfbadled); 
+        flash(RF_BAD_LED); 
     }
     lastmillivolts = readVcc();
     radio.startListening();
     radio.powerDown();
     
-    if (w.millivolts < 2200) {
-        flash(lobattled);
+    if (m.s.millivolts < 2200) {
+        flash(LO_BATT_LED);
     }
     
     // power down for 56 seconds
@@ -156,9 +196,9 @@ void rflog(char * msg) {
   
   bool ok = radio.write(msg, 32);
   if (ok) {
-    flash(rfgoodled);
+    flash(RF_GOOD_LED);
   } else {
-    flash(rfbadled); 
+    flash(RF_BAD_LED); 
   }
   radio.startListening();
 }
@@ -192,34 +232,84 @@ long readVcc() {
 // read jumpers and get node address/id
 int getMyAddr() {
     int result = 0;
-    pinMode(5, INPUT_PULLUP);
-    pinMode(6, INPUT_PULLUP);
-    pinMode(7, INPUT_PULLUP);
+    pinMode(ADDRESS_PIN_0, INPUT_PULLUP);
+    pinMode(ADDRESS_PIN_1, INPUT_PULLUP);
+    pinMode(ADDRESS_PIN_2, INPUT_PULLUP);
     
-    if(!digitalRead(7)) {
+    if(!digitalRead(ADDRESS_PIN_2)) {
         result = result + 4;  
     }
     
-    if(!digitalRead(6)) {
+    if(!digitalRead(ADDRESS_PIN_1)) {
         result = result + 2;  
     }
     
-    if(!digitalRead(5)) {
+    if(!digitalRead(ADDRESS_PIN_0)) {
         result = result + 1 ;  
     }
     
     // save power
-    pinMode(5, OUTPUT);
-    digitalWrite(5, LOW);
-    pinMode(6, OUTPUT);
-    digitalWrite(6, LOW);
-    pinMode(7, OUTPUT);
-    digitalWrite(7, LOW);
+    pinMode(ADDRESS_PIN_0, OUTPUT);
+    digitalWrite(ADDRESS_PIN_0, LOW);
+    pinMode(ADDRESS_PIN_1, OUTPUT);
+    digitalWrite(ADDRESS_PIN_1, LOW);
+    pinMode(ADDRESS_PIN_2, OUTPUT);
+    digitalWrite(ADDRESS_PIN_2, LOW);
     
     return result; 
 }
 
-bool key_is_empty(byte * key) {
+int getMyChannel() {
+    int result = 0;
+    pinMode(CHANNEL_PIN_0, INPUT_PULLUP);
+    pinMode(CHANNEL_PIN_1, INPUT_PULLUP);
+    pinMode(CHANNEL_PIN_2, INPUT_PULLUP);
+    pinMode(CHANNEL_PIN_3, INPUT_PULLUP);
+    
+    if(!digitalRead(CHANNEL_PIN_3)) {
+        result = result + 8;  
+    }
+    
+    if(!digitalRead(CHANNEL_PIN_2)) {
+        result = result + 4;  
+    }
+    
+    if(!digitalRead(CHANNEL_PIN_1)) {
+        result = result + 2;  
+    }
+    
+    if(!digitalRead(CHANNEL_PIN_0)) {
+        result = result + 1 ;  
+    }
+    
+    // save power
+    pinMode(CHANNEL_PIN_0, OUTPUT);
+    digitalWrite(CHANNEL_PIN_0, LOW);
+    pinMode(CHANNEL_PIN_1, OUTPUT);
+    digitalWrite(CHANNEL_PIN_1, LOW);
+    pinMode(CHANNEL_PIN_2, OUTPUT);
+    digitalWrite(CHANNEL_PIN_2, LOW);
+    pinMode(CHANNEL_PIN_3, OUTPUT);
+    digitalWrite(CHANNEL_PIN_3, LOW);
+    
+    return result;
+}
+
+boolean shouldEncrypt() {
+    boolean result = false;
+    pinMode(ENCRYPT_PIN, INPUT_PULLUP);
+    
+    if(!digitalRead(ENCRYPT_PIN)) {
+        result = true;   
+    }
+    
+    pinMode(ENCRYPT_PIN, OUTPUT);
+    digitalWrite(ENCRYPT_PIN, LOW);
+    
+    return result;
+}
+
+bool keyIsEmpty(byte * key) {
     for (int i = 0; i < EEPROM_KEY_SIZE; i++) {
         if (key[i] != 0xff) {
             return false;   
@@ -228,7 +318,7 @@ bool key_is_empty(byte * key) {
     return true;
 }
 
-void generate_key(byte * key) {
+void generateKey(byte * key) {
    randomSeed(analogRead(0));
    for (int i = 0; i < EEPROM_KEY_SIZE; i++) {
        key[i] = (unsigned char) random(0,255);  
