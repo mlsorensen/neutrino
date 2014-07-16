f7eb110f12daa2a7c0bc38e84216a11e559f522f
Marcus Sorensen <marcus@betterservers.com>
2014-07-16 16:18:57 -0600

    renamed cencode to b64encode, added to makefile

--- hub/libs/base64/b64encode.c
+++ hub/libs/base64/b64encode.c
@@ -1,11 +1,11 @@
 /*
-cencoder.c - c source to a base64 encoding algorithm implementation
+b64encoder.c - c source to a base64 encoding algorithm implementation
 
 This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
-#include "cencode.h"
+#include "b64encode.h"
 #include <string.h>
 #include <stdlib.h>
 
--- hub/libs/base64/b64encode.h
+++ hub/libs/base64/b64encode.h
@@ -1,5 +1,5 @@
 /*
-cencode.h - c header for a base64 encoding algorithm
+b64encode.h - c header for a base64 encoding algorithm
 
 This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
